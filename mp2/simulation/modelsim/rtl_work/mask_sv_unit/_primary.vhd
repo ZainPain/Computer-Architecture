a66eea6025bb6e7c0b45c8f5ec18344a987ccca3
ZainPain <zainpaya1995@gmail.com>
2017-10-03 00:58:04 -0500

    removed mp2 shit

--- mp2/simulation/modelsim/rtl_work/mask_sv_unit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mask_sv_unit is
-end mask_sv_unit;
--- mp2/simulation/modelsim/rtl_work/truncator/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity truncator is
-    port(
-        sel             : in     vl_logic;
-        \in\            : in     vl_logic_vector(15 downto 0);
-        \out\           : out    vl_logic_vector(15 downto 0)
-    );
-end truncator;
